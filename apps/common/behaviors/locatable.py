0ee950a3415603eb0941539d6ea553a8a8b31c72
Tom Counsell <other@tomcounsell.com>
2020-06-05 10:08:23 +0700

    add timezone to locatable

--- apps/common/behaviors/locatable.py
+++ apps/common/behaviors/locatable.py
@@ -1,9 +1,11 @@
 from django.db import models
+from timezone_field import TimeZoneField
 
 
 class Locatable(models.Model):
 
     address = models.ForeignKey('common.Address', null=True, blank=True, on_delete=models.SET_NULL)
+    timezone = TimeZoneField()
 
     longitude = models.FloatField(null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
